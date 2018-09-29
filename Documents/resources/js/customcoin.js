db95bb8e9445918ab03d51de43dd3e58e2eb0046
gopalkec24 <30326114+gopalkec24@users.noreply.github.com>
2018-09-28 19:30:11 -0500

    Add files via upload

---
+++ Documents/resources/js/customcoin.js
@@ -0,0 +1,25 @@
+/**
+ * Custom javascript for Coin Dashboard
+ */
+
+function replaceUndefinedData(data)
+{
+	var returnData = data;
+	if(typeof returnData == "undefined")
+	{
+		returnData ="-";
+	}
+	return returnData;
+}
+
+function filterTransactionData($) {
+	var coin = $("#coinName").val().toLowerCase();
+    var exchange = $("#exchangeName").val().toLowerCase();
+    $("#transactionsummary tr").filter(function() {
+    	var toggle= ($(this).text().toLowerCase().indexOf(exchange) > -1) && ($(this).text().toLowerCase().indexOf(coin) > -1);
+    	console.log(toggle);
+      $(this).toggle(toggle);
+    });
+    
+    
+}
